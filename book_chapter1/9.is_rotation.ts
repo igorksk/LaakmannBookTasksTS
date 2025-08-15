fc1e5ea3b4ffa667d4c2731e38e67b9d640b6d9a
Igor Kaseko <igorksk@gmail.com>
2025-08-15 13:56:56 +0300

    is rotation

---
+++ book_chapter1/9.is_rotation.ts
@@ -0,0 +1,24 @@
+import readline from "readline";
+
+function isRotation(s1: string, s2: string): boolean {
+  if (s1.length !== s2.length) return false;
+  const combined = s1 + s1;
+  return combined.includes(s2);
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Read inputs sequentially
+rl.question("Enter string 1: ", (str1) => {
+  rl.question("Enter string 2: ", (str2) => {
+    if (isRotation(str1, str2)) {
+      console.log("Strings are rotations of each other");
+    } else {
+      console.log("Strings are not rotations of each other");
+    }
+    rl.close();
+  });
+});
